4644aea46053c2621b3eed33c18c97fa18c90229
AmanKalabay <149198402+Amansdfg@users.noreply.github.com>
2024-07-17 21:23:10 +0500

    commit

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,49 +1,20 @@
-// src/utils/axios.js
 import axios from 'axios';
-
 const token = localStorage.getItem('token');
 if (token) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 }
-
 axios.interceptors.response.use(
     response => response,
     error => {
         if (error.response && error.response.status === 401) {
-            // Handle 401 Unauthorized errors here (e.g., redirect to login)
             localStorage.removeItem('token');
             window.location.href = '/login';
+        }
+        if(error.response.status === 403){
+            console.log("403")
         }
         return Promise.reject(error);
     }
 );
 
 export default axios;
-// import axios from 'axios';
-//
-// axios.interceptors.request.use(
-//     (config) => {
-//         const token = localStorage.getItem('token');
-//         if (token) {
-//             config.headers.Authorization = `Bearer ${token}`;
-//         }
-//         return config;
-//     },
-//     (error) => {
-//         return Promise.reject(error);
-//     }
-// );
-//
-// axios.interceptors.response.use(
-//     response => response,
-//     error => {
-//         if (error.response && error.response.status === 401) {
-//             // Handle 401 Unauthorized errors here (e.g., redirect to login)
-//             localStorage.removeItem('token');
-//             window.location.href = '/login';
-//         }
-//         return Promise.reject(error);
-//     }
-// );
-//
-// export default axios;
